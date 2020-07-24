3e1bd8a3be1b4ec5aa17d621e421769bd509e9aa
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2020-07-24 14:29:03 -0300

    app profiles created

--- auline/urls.py
+++ auline/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('publications.urls')),
     path('accounts/', include('accounts.urls')),
+    path('perfil/', include('profiles.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
