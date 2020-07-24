0727a847c929aa5073127f028abee848eacd9b0f
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2020-07-24 14:37:37 -0300

    def perfil added

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('animais', perfil, name='animais'),
     path('alterar-informacoes/<int:id>/', alterar, name='alterar-informacoes'),
     path('informacoes-pessoais', informacoes, name='informacoes-pessoais'),
-    path('excluir-conta/<int:id>/', excluir_conta, name='excluir-conta'),
     path('excluir-animal/<int:id_animal>/', excluir_animal, name='excluir-animal'),
     path('editar-animal/<int:id_animal>/', editar_animal, name='editar-animal'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from publicacoes.models import Animal
+from django.contrib.auth.models import User
+from users.models import Perfil
+from users.forms import UserForm, PerfilForm
+from publicacoes.forms import AnimalForm
+from django.contrib.auth.decorators import login_required 
 
-# Create your views here.
+@login_required
+def perfil(request):
+    categorias = {}
+    ids = []
+    animais = Animal.objects.filter(usuario=request.user.id)
+    usuario = User.objects.get(id=request.user.id)
+    perfil_usuario = Perfil.objects.filter(user=request.user.id).first
+
+    for animal in animais:
+        categorias[animal.id] = animal.categoria
+        ids.append(animal.id)
+    
+    contexto = {
+        'animais':animais,
+        'usuario':usuario,
+        'perfil_usuario':perfil_usuario,
+        'ids':ids,
+        'categorias':categorias,
+    }
+    
+    return render(request, 'perfil.html', contexto)
