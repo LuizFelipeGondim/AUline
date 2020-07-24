8f4d5a57655f634c9e49f0c1b5c8bec25a5a6028
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2020-07-24 04:55:09 -0300

    def animal_list created and index.html organized to list animals and filter them

--- publications/views.py
+++ publications/views.py
@@ -8,8 +8,64 @@ from django.core.paginator import Paginator
 
 
 def lista_animal(request):
+    categorias = {}
+    ids = []
+    nome = ''
+    categoria_filtro = ''
+    sexo = ''
+    porte = ''
+    cidade = ''
     
-    return render(request, 'index.html')
+    perfil_usuarios = Perfil.objects.all()
+    usuarios = User.objects.all()
+    lista_de_animais = Animal.objects.all()
+
+    #filtro de animais
+    if request.method == 'POST':
+        if request.POST.get('nome'):
+            nome = request.POST.get('nome')
+            lista_de_animais = lista_de_animais.filter(nome=nome)
+
+        if request.POST.get('categoria_filtro'):
+            categoria_filtro = request.POST.get('categoria_filtro')
+            lista_de_animais = lista_de_animais.filter(categoria=categoria_filtro)
+
+        if request.POST.get('sexo'):
+            sexo = request.POST.get('sexo')
+            lista_de_animais = lista_de_animais.filter(sexo=sexo)
+
+        if request.POST.get('cidade'):
+            cidade = request.POST.get('cidade')
+            lista_de_animais = lista_de_animais.filter(cidade=cidade)
+
+        if request.POST.get('porte'):
+            porte = request.POST.get('porte')
+            lista_de_animais = lista_de_animais.filter(porte=porte)
+    
+    #paginação das publicações
+    paginator = Paginator(lista_de_animais, 15)
+    page = request.GET.get('page')
+    animais = paginator.get_page(page)
+    
+    #transformando as categorias em dicionário para trabalhar com javascript
+    for animal in animais:
+        categorias[animal.id] = animal.categoria
+        ids.append(animal.id)
+
+    contexto = {
+        'nome':nome,
+        'categoria_filtro':categoria_filtro,
+        'cidade':cidade, 
+        'sexo':sexo,
+        'porte':porte,
+        'animais': animais,
+        'perfil_usuarios':perfil_usuarios,
+        'usuarios':usuarios,
+        'categorias': categorias,
+        'ids': ids,
+    }
+    return render(request, 'index.html', contexto)
+
 
 @login_required
 def cadastro_animal(request):
