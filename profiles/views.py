59564c48410ffd7394d97f0c1258612b2a7d2a55
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2020-10-02 00:34:28 -0300

    perfil page finalized

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
-from publications.models import Animal
+from publications.models import Animal, MotivoCadastro
 from django.contrib.auth.models import User
 from accounts.models import Perfil
 from accounts.forms import UserForm, PerfilForm
-from publications.forms import AnimalForm
+from publications.forms import AnimalForm, MotivoForm
 from django.contrib.auth.decorators import login_required 
 
 @login_required
@@ -32,20 +32,18 @@ def perfil(request):
 def alterar(request, id):
     usuario = User.objects.get(id=request.user.id)
     perfil_usuario = Perfil.objects.get(usuario=request.user.id)
-    form = UserForm(request.POST or None, instance=usuario)
     form_perfil = PerfilForm(request.POST or None, request.FILES or None, 
                             instance=perfil_usuario)
     contexto = {
         'usuario':usuario,
         'perfil_usuario':perfil_usuario,
-        'form':form,
         'form_perfil':form_perfil,
     }
     if request.method == 'POST':
         if form_perfil.is_valid():
-            edit = form_perfil.save(commit=False)
-            edit.save()
-            return redirect('informacoes-pessoais')
+            form_perfil.save()
+
+            return redirect('animais')
         
     return render(request,'alterar-informacoes.html', contexto)
 
@@ -58,23 +56,27 @@ def excluir_animal(request, id_animal):
 @login_required
 def editar_animal(request, id_animal):
     animal = Animal.objects.get(id=id_animal)
+    motivo = MotivoCadastro.objects.get(animal_id=id_animal)
     usuario = User.objects.get(id=request.user.id)
     perfil_usuario = Perfil.objects.get(usuario=request.user.id)
+
+    form_motivo = MotivoForm(request.POST or None, instance=motivo)
     form = AnimalForm(request.POST or None, request.FILES or None,
                     instance=animal)
-    if request.user == animal.usuario:
-        contexto = {
-            'usuario':usuario,
-            'perfil_usuario':perfil_usuario,
-            'form':form,
-            'animal':animal,
-        }
-        if request.method == 'POST':
-            if form.is_valid():
-                edit = form.save(commit=False)
-                edit.save()
-                return redirect('animais')
 
-        return render(request, 'editar-animal.html', contexto)
-    else:
-        return redirect('animais')
+        
+    if request.method == 'POST':
+        if form.is_valid() and form_motivo.is_valid():
+            form_motivo.save()
+            form.save()
+            return redirect('animais')
+
+    contexto = {
+        'usuario':usuario,
+        'perfil_usuario':perfil_usuario,
+        'form':form,
+        'form_motivo':form_motivo,
+        'animal':animal,
+    }
+
+    return render(request, 'editar-animal.html', contexto)
