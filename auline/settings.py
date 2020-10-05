eeb66c41e0910c0a0576fa9c7a519848e0142f00
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2020-10-05 15:30:24 -0300

    change database

--- auline/settings.py
+++ auline/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'auline.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
@@ -85,6 +85,13 @@ DATABASES = {
         'PASSWORD': 'postgres',
         'HOST': 'db',
         'PORT': 5432,
+    }
+}'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
